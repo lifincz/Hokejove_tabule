2dbaa84e2af8ec7e513d95394c736dded5682668
lifincz <61046127+lifincz@users.noreply.github.com>
2024-10-09 18:47:03 +0200

    add BLE comm
    Added BLE receive and send. Disabled prints of acceleration.

--- esp32_firmware/esp32_firmware.ino
+++ esp32_firmware/esp32_firmware.ino
@@ -1,12 +1,24 @@
 #include <Adafruit_NeoPixel.h>
 #include "MPU9250.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <string>
+
 #define MPU9250_ADDRESS 0x68
-#define ACCEL_CONFIG 0x1C
+#define ACCEL_CONFIG 0x1C // Register for changing the accelerometer range
 
 #define PIN_LED 4
 #define NUMLED  32 // 12 + 12 + 4 + 4 LEDs
 
-#define SW_VERSION "0.0.1_23.09.2024"
+#define SW_VERSION "0.0.2_09.10.2024"
+
+// ESP32 Wifi/BT comm with phone app
+// BLE UUID for communication
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
 float AccX = 0;
 float AccY = 0;
@@ -16,20 +28,98 @@ int color = 0;
 float lastAccMax = 0;
 bool rising = 0;
 
+bool deviceConnected      = 0;      // if BLE device is connected
+float txValue             = 0;
+
+BLECharacteristic *pCharacteristic;
+std::string prijataZprava;
+
 //Adafruit_NeoPixel pixels(NUMLED, PIN_LED, NEO_GRBW + NEO_KHZ400); // for SK6812 RGBW ideal
 Adafruit_NeoPixel pixels(NUMLED, PIN_LED, NEO_GRBW + NEO_KHZ400); // for WS2812 RGB chip
 // pixels.setPixelColor(j, pixels.Color(0, 1, 0, 0)); // R, G, B, W
 
 MPU9250 mpu;
 
+class MyServerCallbacks: public BLEServerCallbacks {        // Class for checking BLE connection
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = 1;
+      Serial.println("Device connected...");
+    };
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = 0;
+      Serial.println("Device disconnected...");
+      pServer->getAdvertising()->start();   // aby se to pri kazdem odpojeni clienta zase zacalo nabizet k pripojeni
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {      // Callback for BLE messages
+  void onRead(BLECharacteristic *pCharacteristic) {
+    char txString[12];
+    dtostrf(txValue, 1, 2, txString);                       // float_val, min_width, digits_after_decimal, char_buffer
+    pCharacteristic->setValue(txString);
+  }
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    String rxMsg = pCharacteristic->getValue();
+    std::string rxValue = rxMsg.c_str();  // Convert String to std::string
+
+  //  String rxMsg = rxValue.c_str(); // for future avoid String, use char array istead to avoid possible heap fragmentation
+    if (rxValue.length() > 0) {
+      Serial.print("BLE Rx: ");
+      for (int i = 0; i < rxValue.length(); i++) {
+        Serial.print(rxValue[i]);
+      }
+      Serial.println();
+
+      // Do stuff based on the command received from the app
+      if (rxMsg.startsWith("RESTART_ALL") == 1)   {
+        Serial.println("STRING1");
+        }
+      else if (rxMsg.startsWith("RESTART_TIME") == 1)   {
+        Serial.println("STRING2");
+        }
+    }
+  }
+};
+
+void send_message_BLE(char txString[]){
+  Serial.print("*** BLE Sent value: ");
+  pCharacteristic->setValue(txString);
+  Serial.println(txString);
+  pCharacteristic->notify();                      // Send the value to the app via BLE
+}
+
+void initialize_BLE(){
+  BLEDevice::init("HockeyTable_1");
+  BLEServer *pServer = BLEDevice::createServer();                         // create BLE Server
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);            // create BLE service
+  pCharacteristic = pService->createCharacteristic(                       // vytvoření BLE komunikačního kanálu pro odesílání (TX)
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
+                    ); 
+  pCharacteristic->addDescriptor(new BLE2902());
+  pCharacteristic->setCallbacks(new MyCallbacks());                       // assign callback function
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(    // vytvoření BLE komunikačního kanálu pro prijimani (RX)
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  
+  pService->start();                                                      // Turn ON BLE
+  pServer->getAdvertising()->start();                                     // Device visibility for BLE enabled
+
+  esp_err_t errRc = esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9); // Set BLE power to maximum
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN ,ESP_PWR_LVL_P9); 
+  }
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
   Wire.beginTransmission(MPU9250_ADDRESS);
-    Wire.write(ACCEL_CONFIG);
-    Wire.write(0x18); // 0x18 pro ±16g, 0x00 pro 2g
-    Wire.endTransmission();
+  Wire.write(ACCEL_CONFIG);
+  Wire.write(0x18); // 0x18 pro ±16g, 0x00 pro 2g
+  Wire.endTransmission();
   delay(1000);
   pixels.begin();
   pixels.clear();
@@ -40,6 +130,7 @@ void setup() {
         delay(5000);
     }
   }
+  initialize_BLE();
   
 
 }
@@ -80,7 +171,6 @@ void loop() {
   }
 
   lastAccMax = AccMax; 
-  delay(5);
 }
 
 
@@ -88,7 +178,7 @@ void print_roll_pitch_yaw() {
     AccX = mpu.getAccX();
     AccY = mpu.getAccY();
     AccZ = mpu.getAccZ();
-    AccMax = max(AccY, AccY, AccZ);
+    AccMax = max(max(AccY, AccY), AccZ);
     Serial.print(AccX, 2);
     Serial.print(", ");
     Serial.print(AccY, 2);
